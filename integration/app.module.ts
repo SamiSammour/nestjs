8c98a868d5a17b48e637ae6809f0434e627fb658
Dania801 <ssansnnom07@gmail.com>
2019-12-28 10:49:35 +0200

    Update: export AuthUser and test integration

--- integration/app.module.ts
+++ integration/app.module.ts
@@ -5,13 +5,27 @@ import { DatabaseModule } from '@alphaapps/nestjs-db';
 import { ConfigModule } from '@alphaapps/nestjs-config';
 import { UserModule } from './users/user.module';
 import schema from './defaults/configSchema';
+import { AuthModule } from '@alphaapps/nestjs-auth';
+import User from './users/user.model';
 
 @Module({
   imports: [
     ConfigModule.register({ schema }),
     DatabaseModule.register({ modelsDir: [join(dirname(require.main.filename), './**/*.model.ts')] }),
     AccessControlModule.forRoles(new RolesBuilder()),
-    UserModule
+    UserModule,
+    AuthModule.register({
+      userModel: User,
+      expiresIn: 0,
+      oneSessionPerAccount: false,
+      loginMethods: [{
+        loginField: 'email',
+        passwordField: 'password'
+      }, {
+        loginField: 'phoneNumber',
+        passwordField: 'pin'
+      }]
+    }),
   ],
 })
 export class AppModule {}
--- packages/nestjs-auth/src/index.ts
+++ packages/nestjs-auth/src/index.ts
@@ -1,2 +1,3 @@
 export * from './module/auth.module';
 export * from './module/auth.service';
+export * from './dto/authUser.interface';
