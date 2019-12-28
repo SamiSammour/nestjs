2b2031e758cc2b78b19a5a83198cf0b513003b5a
Dania801 <ssansnnom07@gmail.com>
2019-12-28 11:03:20 +0200

    Fix: remove API keys

--- packages/nestjs-auth/src/index.ts
+++ packages/nestjs-auth/src/index.ts
@@ -1,3 +1,5 @@
 export * from './module/auth.module';
 export * from './module/auth.service';
+export * from './dto/register-user.dto';
 export * from './dto/authUser.interface';
+export * from './jwt/jwt-payload.interface';
